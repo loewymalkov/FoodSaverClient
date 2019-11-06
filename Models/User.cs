5659e97814d06819a0daede2fad3af008a76e2d5
Student <example@example.com>
2019-11-06 12:10:18 -0800

    add shopping list details

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,14 @@
+using System.Collections.Generic;
+
+
 namespace FoodSaverClient.Models
 {
     public class User
     {
         public int Budget { get; set; }
         public int ServingQuantity { get; set; }
-        public int RecipeId { get; set; }
         public decimal ShoppingListTotal { get; set; }
+        public List<Recipe> RecipeId { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
     }
 }
